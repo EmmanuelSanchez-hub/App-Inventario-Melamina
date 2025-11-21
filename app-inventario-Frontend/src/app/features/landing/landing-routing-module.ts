77c4b5631768127e6ede14d8c400449f645fed73
Emmanuel Sanchez Velasquez <u20216260@utp.edu.pe>
2025-11-21 02:06:13 -0500

    Se agrega Landing Page completa con presentación de servicios y botones de acción

--- app-inventario-Frontend/src/app/features/landing/landing-routing-module.ts
+++ app-inventario-Frontend/src/app/features/landing/landing-routing-module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Landing } from './pages/landing/landing';
+import { Contactar } from './pages/contactar/contactar';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: Landing },
+  { path: 'contactar', component: Contactar }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
